20814d5674a4ce9403df4beef60297cff72c36a0
Andres Giraldo <andresganc@gmail.com>
2023-04-08 10:42:19 -0500

    New: Course NC - Arrays Methods - Some Numbers

--- course-nc/2-advanced/code/arrays-methods/find/exercises/find-number.js
+++ course-nc/2-advanced/code/arrays-methods/find/exercises/find-number.js
@@ -1,6 +1,6 @@
 
 const numbers = [ 4, 2, 3, 5, 8, 9, 15, 657]
 
-// Correct way
+// Correct way to find a number (Do not use filter)
 const findNumber = numbers.find(n => n === 15)
 console.log(findNumber)
