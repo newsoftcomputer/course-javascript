ab75620ea50a4f6ca38adc6f975283c762508726
Andres Giraldo <andresganc@gmail.com>
2023-04-08 11:28:35 -0500

    Mod: Course NC - Arrays Methods - indexOf Names

--- course-nc/2-advanced/code/arrays-methods/index-of/examples/indexof-numbers-01.js
+++
@@ -1,8 +0,0 @@
-
-
-const numbers = [ 4, 2, 3, 5, 8, 9, 15, 657]
-
-const someNumber = numbers.some(n => n === 15)
-console.log(someNumber)
-
-// Result : true
---
+++ course-nc/2-advanced/code/arrays-methods/index-of/examples/indexof-numbers.js
@@ -0,0 +1,8 @@
+
+
+const numbers = [ 4, 2, 3, 5, 8, 9, 15, 657]
+
+const indexOfNumber = numbers.indexOf(15)
+console.log(indexOfNumber)
+
+// Result : 6 (position inside the array)
