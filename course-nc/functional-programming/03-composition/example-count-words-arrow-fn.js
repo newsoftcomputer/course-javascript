d5302231b4fb21bfc425efc38a051c1dab7ef90f
Andres Giraldo <andresganc@gmail.com>
2023-06-16 11:19:35 -0500

    Mod: Functional programing - Composition

--- course-nc/functional-programming/03-composition/example-count-words-arrow-fn.js
+++ course-nc/functional-programming/03-composition/example-count-words-arrow-fn.js
@@ -5,27 +5,21 @@
 const text = 'Learn to programing web application'
 
 // Fn to separate words
-const splitSpace = function(str) {
-    return str.split(' ')
-}
+const splitSpace = (str) => str.split(' ')
 
 const testSplit = splitSpace(text)
 console.log(testSplit)
 
 
 // Fn to count arrays
-const count = function(arr) {
-    return arr.length
-}
+const count = (arr) => arr.length
 
 const testCount = count(testSplit)
 console.log(testCount)
 
 
 // Composition from fn splitSpace and fn count
-const countWords = function(str){
-    return count(splitSpace(str))
-}
+const countWords = (str) => count(splitSpace(str))
 
 const result = countWords(text)
 console.log(result)
