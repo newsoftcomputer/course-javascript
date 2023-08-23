a50c68253bf45eb8fafb0c6d9236638eb35fa08e
Andres Giraldo <andresganc@gmail.com>
2023-08-22 19:37:20 -0500

    New: Location Object, History Object, Navigator Object

---
+++ course-nc/bom/04-location/location.js
@@ -0,0 +1,17 @@
+
+// OBJECT URL (LOCATION)
+
+console.log('location :', location)
+console.log('location.origin :', location.origin)
+console.log('location.protocol :', location.protocol)
+console.log('location.host :', location.host)
+console.log('location.hostname :', location.hostname)
+console.log('location.port :', location.port)
+console.log('location.href :', location.href)
+console.log('location.hash :', location.hash)
+console.log('location.pathname :', location.pathname)
+console.log('location.search :', location.search)
+
+// Methods Reload from Object Location (Page Reload)
+// location.reload()
+
---
+++ course-nc/bom/06-navigator/navigator.js
@@ -0,0 +1,2 @@
+
+console.log('Navigator Object :', navigator)
