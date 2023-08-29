ebd30e05049315d72c6b63db788825efab5a5454
Andres Giraldo <andresganc@gmail.com>
2023-08-29 11:36:23 -0500

    New: Const, DOM: Attributes

--- course-nc/dom/course-nc/5-attributes/attributes.js
+++ course-nc/dom/course-nc/5-attributes/attributes.js
@@ -6,3 +6,12 @@ console.log(document.documentElement.getAttribute('lang'))
 // 2 formas
 console.log(document.querySelector('.link-dom').href)   // Muestra la ruta completa
 console.log(document.querySelector('.link-dom').getAttribute('href'))   // Muestra solo el valor en href
+
+// Attributes HTML-DocumentElement
+document.documentElement.lang = 'en'
+console.log(document.documentElement.lang)
+
+// Set Attribute
+document.documentElement.setAttribute('lang', 'es-MX')
+console.log(document.documentElement.lang)
+
