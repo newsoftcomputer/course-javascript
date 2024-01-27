b6db312babf2c71096ff2b551287845f2dadc3fb
Andres Giraldo <andresganc@gmail.com>
2024-01-27 12:17:17 -0500

    Mod: Exercises: Todo Lsit Advanced

--- exercises/01-basic/exercises-todo-list/src/todo-list-advanced/TodoListAdvanced.js
+++ exercises/01-basic/exercises-todo-list/src/todo-list-advanced/TodoListAdvanced.js
@@ -41,10 +41,32 @@ buttonTask.addEventListener('click', (e) => {
     console.log(arrayTasks)
 
     document.querySelector('#id_tasks_adv').innerHTML = 
-    arrayTasks.map( x => {
-        `<div>
-            <p>${x}</p>
-        </div>
+    arrayTasks.map( x => 
         `
-    }).join()
+        <div class='${styles.tla__tasks_adv_cont}'>
+            <div>
+                <p key='${x}'>${x}</p>
+            </div>
+            
+            <a id='id_delete' key='${x}' class='${styles.tla__tasks_adv_delete}'>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="25" fill='#D50000'> <title>Delete Task</title><path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" /></svg>
+            </a>
+
+            <a id='id_edit' class='${styles.tla__tasks_adv_edit}'>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="25" fill='#76FF03'> <title>Edit Task</title><path d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" /></svg>
+            </a>
+            
+        </div>
+        
+        `
+    ).join('')
+
+    let inputTask = document.querySelector('#input_task_adv')
+    inputTask.value = ''
+})
+
+let buttonDelete = document.querySelector('#id_delete')
+buttonDelete.addEventListener('click', () => {
+
+    console.log('Delete')
 })
