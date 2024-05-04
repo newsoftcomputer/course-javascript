8120d9b796d60d704e3cd8bd4d267b1421134eeb
Andres Giraldo <andresganc@gmail.com>
2024-05-04 12:37:03 -0500

    New: Arrays: At: Comparing Methods, Calling at on non array objects

---
+++ course-nc/02-advanced/code/01-arrays-advanced/04-array-instance-methods/at/examples/03-comparing-methods.js
@@ -0,0 +1,15 @@
+
+// Our array with items
+const colors = ["red", "green", "blue"];
+
+// Using length property
+const lengthWay = colors[colors.length - 2];
+console.log(lengthWay); // 'green'
+
+// Using slice() method. Note an array is returned
+const sliceWay = colors.slice(-2, -1);
+console.log(sliceWay[0]); // 'green'
+
+// Using at() method
+const atWay = colors.at(-2);
+console.log(atWay); // 'green'
