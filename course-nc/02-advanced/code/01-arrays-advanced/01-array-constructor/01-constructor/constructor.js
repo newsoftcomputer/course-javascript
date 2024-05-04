1a09a9a89901f3deffb62a85c5d2d96db57a9321
Andres Giraldo <andresganc@gmail.com>
2024-05-04 11:28:01 -0500

    New: Arrays: Constructor, Literal Notation, With Single Parameter, With multiple parameter

---
+++ course-nc/02-advanced/code/01-arrays-advanced/01-array-constructor/01-constructor/constructor.js
@@ -0,0 +1,12 @@
+
+new Array()
+new Array(element1)
+new Array(element1, element2)
+new Array(element1, element2, /* …, */ elementN)
+new Array(arrayLength)
+
+Array()
+Array(element1)
+Array(element1, element2)
+Array(element1, element2, /* …, */ elementN)
+Array(arrayLength)
---
+++ course-nc/02-advanced/code/01-arrays-advanced/01-array-constructor/02-literal-notation/literal-notation.js
@@ -0,0 +1,5 @@
+
+const fruits = ["Apple", "Banana"];
+
+console.log(fruits.length); // 2
+console.log(fruits[0]); // "Apple"
