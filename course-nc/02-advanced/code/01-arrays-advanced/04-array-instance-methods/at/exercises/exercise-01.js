0fd506d010285a92d6e30ebd7fe497aba9ced5ad
Andres Giraldo <andresganc@gmail.com>
2024-05-11 10:31:03 -0500

    New: Course: Arrays: At: Exercise 01

---
+++ course-nc/02-advanced/code/01-arrays-advanced/04-array-instance-methods/at/exercises/exercise-01.js
@@ -0,0 +1,15 @@
+
+'use strict'
+
+const indexArray = (index) => {
+
+    const array01 = ['Marco', 'Mariana', 'Daniela', 'Alejandra', 'Andres']
+
+    let name = array01.at(index)
+    
+    return name 
+
+}
+
+
+console.log(indexArray(0))
